104af1401d772633054f63e13ee27659e62cd831
Tiago Alexandre Plautz <tiago.plautz@paytrack.com.br>
2026-09-21 01:11:31 -0300

    Adicionado validações extras de campos no requestDto e ajsutado os teste unitários

--- backend/src/main/java/br/com/paytrack/testeTecnico/cartao/dto/CartaoRequestDTO.java
+++ backend/src/main/java/br/com/paytrack/testeTecnico/cartao/dto/CartaoRequestDTO.java
@@ -3,6 +3,7 @@ package br.com.paytrack.testeTecnico.cartao.dto;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.OffsetDateTime;
@@ -22,6 +23,7 @@ public class CartaoRequestDTO {
     private String identificador;
 
     @NotBlank(message = "Número do cartão é obrigatório")
+    @Pattern(regexp = "\\d{16}", message = "O número do cartão deve conter exatamente 16 números")
     private String numeroCartao;
 
     @NotNull(message = "Data de validade é obrigatório")
@@ -30,7 +32,8 @@ public class CartaoRequestDTO {
 
     private String bandeira;
 
-    @NotNull
+    @NotBlank(message = "O campo CVV é obrigatório")
+    @Pattern(regexp = "\\d{3,4}", message = "O campo CVV deve conter somente números e entre 3 a 4 caracteres")
     private String cvv;
 
 }
